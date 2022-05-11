1baa014d73f6709fd71af2cbb9532f8335103f5e
AmandaMariaFS <amandinhaferre@hotmail.com>
2022-05-11 10:58:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.Sqlite;
+
+var connection = new SqliteConnection("Data Source=database.db");
+connection.Open();
+
+var command = connection.CreateCommand();
+command.CommandText = @"
+    CREATE TABLE IF NOT EXISTS Computers(
+        id int not null primary key,
+        ram varchar(100) not null,
+        processador varchar(100) not null
+    );
+";
+
+command.ExecuteNonQuery();
+
+connection.Close();
+
+var modelName = args[0];
+var modelAction = args[1];
+
+if(modelName == "Computer")
+{
+    if(modelAction == "List")
+    {
+        Console.WriteLine("List Computer");
+    }
+
+    if(modelAction == "New")
+    {
+        var id = Convert.ToInt32(args[2]);
+        var ram = args[3];
+        var processador = args[4];
+        Console.WriteLine("New Computer");
+        Console.WriteLine("{0}, {1}, {2}", id, ram, processador);
+    }
+}
