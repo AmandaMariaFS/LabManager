354ee1e852d4e6c4b59bca84244d56741aa1ff36
AmandaMariaFS <amandinhaferre@hotmail.com>
2022-05-28 17:15:12 -0300

    Implementação dos métodos GetById e Save em LabRepository

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using LabManager.Models;
 var databaseConfig = new DatabaseConfig();
 var databaseSetup = new DatabaseSetup(databaseConfig);
 var computerRepository = new ComputerRepository(databaseConfig);
+var labRepository = new LabRepository(databaseConfig);
 
 var modelName = args[0];
 var modelAction = args[1];
@@ -60,43 +61,22 @@ if(modelName == "Lab")
 {
     if(modelAction == "List")
     {
-        Console.WriteLine("List Lab");
-        var connection = new SqliteConnection(databaseConfig.ConnectionString);
-        connection.Open();
-
-        var command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Lab";
-
-        var reader = command.ExecuteReader();
-
-        while(reader.Read())
+        foreach (var Lab in labRepository.GetAll())
         {
-            Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3));
+            Console.WriteLine("{0}, {1}, {2}, {3}", Lab.Id, Lab.Number, Lab.Name, Lab.Block);
         }
-
-        connection.Close();
     }
 
     if(modelAction == "New")
     {
-        var id_lab = Convert.ToInt32(args[2]);
+        var id = Convert.ToInt32(args[2]);
         var number = Convert.ToInt32(args[3]);
         var name = args[4];
         var block = args[5];
-        Console.WriteLine("New Computer");
-        Console.WriteLine("{0}, {1}, {2}, {3}", id_lab, number, name, block);
+        Console.WriteLine("New Lab");
+        Console.WriteLine("{0}, {1}, {2}, {3}", id, number, name, block);
 
-        var connection = new SqliteConnection(databaseConfig.ConnectionString);
-        connection.Open();
-
-        var command = connection.CreateCommand();
-        command.CommandText = "INSERT INTO Computers VALUES($id_lab, $number, $name, $block)";
-        command.Parameters.AddWithValue("$id_lab", id_lab);
-        command.Parameters.AddWithValue("$number", number);  
-        command.Parameters.AddWithValue("$name", name);
-        command.Parameters.AddWithValue("$block", block);
-
-        command.ExecuteNonQuery();
-        connection.Close();
+        var lab = new Lab(id, number, name, block);
+        labRepository.Save(lab);
         }
 }
